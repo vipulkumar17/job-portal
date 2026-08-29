8636cf068aafca73c509185be31d8aa2db8ae4b8
vipulkumar17 <kumarvipul6392@gmail.com>
2026-08-30 00:16:10 +0530

    jwt generation and validation filter

--- src/main/java/com/jobportal/job_portal/config/WebSecurityConfig.java
+++ src/main/java/com/jobportal/job_portal/config/WebSecurityConfig.java
@@ -3,16 +3,24 @@ package com.jobportal.job_portal.config;
 
 
 
+import com.jobportal.job_portal.security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    WebSecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
@@ -28,7 +36,9 @@ public class WebSecurityConfig {
         )
          .sessionManagement(session->session
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-         );
+         )
+
+         .addFilterBefore(jwtAuthFilter,UsernamePasswordAuthenticationFilter.class);
          return http.build();
     }
     
--- src/main/java/com/jobportal/job_portal/security/JwtUtil.java
+++ src/main/java/com/jobportal/job_portal/security/JwtUtil.java
@@ -1,8 +1,4 @@
 package com.jobportal.job_portal.security;
-import org.springframework.stereotype.Component;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.security.Keys;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -23,40 +19,40 @@ public class JwtUtil {
     @Value("${jwt.expiration}")
     private long expiration;
 
-    private SecretKey getSigningKey(){
+    private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(secret.getBytes());
     }
 
-    public String generateToken(String email,String role){
+    public String generateToken(String email, String role) {
         return Jwts.builder()
-        .subject(email)
-        .claim("role", role)
-        .issuedAt(new Date())
-        .expiration(new Date(System.currentTimeMillis() +expiration))
-        .signWith(getSigningKey())
-        .compact();
+                .subject(email)
+                .claim("role", role)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
     }
 
-    public String extractEmail(String token){
-        return extractClaim(token,Claims:: getSubject);
-    }
-    public String extractRole(String token){
-        return extractClaim(token,claims->claims.get("role",String.class));
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
     }
 
-    private<T> T extractClaim(String token, Function<Claims,T> claimsResolver){
-        Claims claims=(Jwts.parser())
-        .build()
-        .parseSignedClaims(token)
-        .getPayload();
+    public String extractRole(String token) {
+        return extractClaim(token, claims -> claims.get("role", String.class));
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
         return claimsResolver.apply(claims);
+    }
 
-    } 
-    public boolean isValid(String email,String token){
-        String tokenEmail=extractEmail(token);
-        Date expirationDate=extractClaim(token,Claims::getExpiration);
+    public boolean isTokenValid(String token, String email) {
+        String tokenEmail = extractEmail(token);
+        Date expirationDate = extractClaim(token, Claims::getExpiration);
         return tokenEmail.equals(email) && expirationDate.after(new Date());
-
     }
 }
- 
